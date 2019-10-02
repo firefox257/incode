ba19d03302dd8e5d961d34d38922daa362403d43
firefox257 <49958313+firefox257@users.noreply.github.com>
2019-10-01 22:45:09 -0600

    add multi name handler

--- msgc.js
+++ msgc.js
@@ -1,37 +1,48 @@
-
 var msgc =(function()
-{
-      var fa = {};
-      return {
-          handle: function(name, fptr)
-          {
-              if(fa[name] == undefined) fa[name] = [];
-              fa[name].push(fptr);
-          },
-          send: function(name)
-          {
-              if(arguments.length>0)
-              {
-                  var cstr = "a[i](arguments[1]";
-                  for(var i = 2; i < arguments.length; i++)
-                  {
-                      cstr = cstr + ", arguments[" + i + "]";
-                  }
-                  cstr = cstr + ");";
-                  var a = fa[name];
-                  for(var i = 0; i < a.length; i++)
-                  {
-                      eval(cstr);
-                  }
-              }//end if
-              else
-              {
-                  var a = fa[name];
-                  for(var i = 0; i < a.length; i++)
-                  {
-                      a[i]();
-                  }
-              }
-          }
-      };
-})();
+	{
+		var fa = {};
+		return {
+			handle: function(name, fptr)
+			{
+				if(typeof name=="string")
+				{
+					if(fa[name] == undefined) fa[name] = [];
+					fa[name].push(fptr);
+					return;
+				}
+				for(n in name)
+				{
+					var nn=name[n];
+					if(fa[nn] == undefined) fa[nn] = [];
+					fa[nn].push(fptr);
+				}
+			},
+			send: function(name)
+			{
+				if(arguments.length>0)
+				{
+					var cstr = "a[i](arguments[1]";
+					for(var i = 2; i < arguments.length; i++)
+					{
+						cstr = cstr + ", arguments[" + i + "]";
+					}
+					cstr = cstr + ");";
+					var a = fa[name];
+					for(var i = 0; i < a.length; i++)
+					{
+						eval(cstr);
+					}
+				}//end if
+				else
+				{
+					var a = fa[name];
+					for(var i = 0; i < a.length; i++)
+					{
+						a[i]();
+					}
+				}
+			}
+		};
+	})();
+$h=msgc.handle;
+$s=msgc.send;
